738cdf8c95be3c514f2eeae4c559c7a19a951e72
Blxee <aminedev57@gmail.com>
2024-01-25 08:45:54 +0100

    Added: task (0. Simple helper function) of 0x00. Pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
-0. Simple helper function
+"""0. Simple helper function"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    start = page_size * (page - 1)
+    end = page_size * page
+    return (start, end)
