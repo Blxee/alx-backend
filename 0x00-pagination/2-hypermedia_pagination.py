43bf1652eda2438e12a3555fd08de8217147627d
Blxee <aminedev57@gmail.com>
2024-01-28 15:18:52 +0100

    Added: task (2. Hypermedia pagination) of 0x00. Pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,2 +1,56 @@
 #!/usr/bin/env python3
-2. Hypermedia pagination
+"""2. Hypermedia pagination"""
+import csv
+import math
+from typing import Dict, List
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns a page from a CSV file."""
+        assert type(page) == int, "page must be an integer"
+        assert type(page_size) == int,  "page_size must be an integer"
+        assert page > 0, "page must be greater than 0"
+        assert page_size > 0,  "page_size must be greater than 0"
+        self.dataset()
+        i, j = index_range(page, page_size)
+        if self.__dataset is None:
+            return []
+        return self.__dataset[i:j]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Returns page from the dataset with additional meta data."""
+        i, j = index_range(page, page_size)
+        data = self.get_page(page, page_size)
+        if self.__dataset is None:
+            return {}
+        next_page = page + 1 if (page + 1) * page_size < len(self.__dataset) else None
+        prev_page = page - 1 if (page - 1) * page_size >= 0 else None
+        total_pages = math.ceil(len(self.__dataset) / page_size)
+        return {
+            'page_size': page_size,
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages,
+        }
