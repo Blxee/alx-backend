913d94cf27074448a9b1fc9a45f37a84d1da60a7
Blxee <aminedev57@gmail.com>
2024-01-28 14:59:25 +0100

    Added: task (1. Simple pagination) of 0x00. Pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,2 +1,38 @@
 #!/usr/bin/env python3
-1. Simple pagination
+"""1. Simple pagination"""
+import csv
+import math
+from typing import List
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns a page from a CSV file."""
+        assert type(page) == int, "page must be an integer"
+        assert type(page_size) == int,  "page_size must be an integer"
+        assert page > 0, "page must be greater than 0"
+        assert page_size > 0,  "page_size must be greater than 0"
+        self.dataset()
+        i, j = index_range(page, page_size)
+        if self.__dataset == None:
+            return []
+        return self.__dataset[i:j]
