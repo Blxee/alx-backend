a68c63a6cccf95ea726a70c393c165d1d09b2145
Blxee <aminedev57@gmail.com>
2024-01-30 18:02:55 +0100

    Added: current progress

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,1 +1,30 @@
-5. LFU Caching
+#!/usr/bin/python3
+"""5. LFU Caching"""
+from heapq import heappush, heappop
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """Implements a FIFO caching system."""
+
+    def __init__(self):
+        super().__init__()
+        self._least_frequently_used = []
+
+    def put(self, key, item):
+        """Puts a new entry into the cache system."""
+        if key is None or item is None:
+            return
+        if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+            key = heappop(self._least_frequently_used)
+            del self.cache_data[key]
+            print('DISCARD:', key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves an entry from the cache system."""
+        if key not in self.cache_data:
+            return None
+        frequency = heappop(self._least_frequently_used)
+        heappush(self._least_frequently_used, (frequency + 1, key))
+        return self.cache_data.get(key)
--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,1 +1,21 @@
-2. LIFO Caching
+#!/usr/bin/python3
+"""2. LIFO Caching"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """Implements a LIFO caching system."""
+
+    def put(self, key, item):
+        """Puts a new entry into the cache system."""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            key = list(self.cache_data.keys())[-2]
+            del self.cache_data[key]
+            print('DISCARD:', key)
+
+    def get(self, key):
+        """Retrieves an entry from the cache system."""
+        return self.cache_data.get(key)
