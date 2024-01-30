eb5b144689b9997a03ad79673d607cb456d41743
Blxee <aminedev57@gmail.com>
2024-01-30 12:37:23 +0100

    Added: task (3. MRU caching) of 0x01. Caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,1 +1,34 @@
-4. MRU Caching
+#!/usr/bin/python3
+"""4. MRU Caching"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """Implements a MRU caching system."""
+
+    def __init__(self):
+        super().__init__()
+        self._most_recently_used = []
+
+    def put(self, key, item):
+        """Puts a new entry into the cache system."""
+        if key is None or item is None:
+            return
+        if key in self._most_recently_used:
+            self._most_recently_used.remove(key)
+        self._most_recently_used.append(key)
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            key = self._most_recently_used[-2]
+            self._most_recently_used.remove(key)
+            del self.cache_data[key]
+            print('DISCARD:', key)
+
+    def get(self, key):
+        """Retrieves an entry from the cache system."""
+        if key not in self.cache_data:
+            return None
+        if key in self.cache_data:
+            self._most_recently_used.remove(key)
+        self._most_recently_used.append(key)
+        return self.cache_data.get(key)
