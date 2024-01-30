90bf44c80aec08862ce9257fd0e719de4f08308e
Blxee <aminedev57@gmail.com>
2024-01-30 11:59:15 +0100

    Added: task (3. LRU caching) of 0x01. Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,1 +1,33 @@
-3. LRU Caching
+#!/usr/bin/python3
+"""3. LRU Caching"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """Implements a LRU caching system."""
+
+    def __init__(self):
+        super().__init__()
+        self._least_recently_used = []
+
+    def put(self, key, item):
+        """Puts a new entry into the cache system."""
+        if key is None or item is None:
+            return
+        if key in self._least_recently_used:
+            self._least_recently_used.remove(key)
+        self._least_recently_used.insert(0, key)
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            key = self._least_recently_used.pop()
+            del self.cache_data[key]
+            print('DISCARD:', key)
+
+    def get(self, key):
+        """Retrieves an entry from the cache system."""
+        if key not in self.cache_data:
+            return None
+        if key in self.cache_data:
+            self._least_recently_used.remove(key)
+        self._least_recently_used.insert(0, key)
+        return self.cache_data.get(key)
