23a7f402b23196edf627244e7ebc9f94f5d9a9b0
Blxee <aminedev57@gmail.com>
2024-02-01 00:35:06 +0100

    Fixed: task (5. LFU caching) of 0x01. Caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -5,7 +5,7 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """Implements a FIFO caching system."""
+    """Implements a LFO caching system."""
 
     def __init__(self):
         super().__init__()
@@ -16,15 +16,19 @@ class LFUCache(BaseCaching):
         if key is None or item is None:
             return
         if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-            key = heappop(self._least_frequently_used)
+            key = heappop(self._least_frequently_used)[1]
             del self.cache_data[key]
             print('DISCARD:', key)
         self.cache_data[key] = item
+        frequency = 0
+        if key in map(lambda x: x[1], self._least_frequently_used):
+            frequency = heappop(self._least_frequently_used)[0]
+        heappush(self._least_frequently_used, (frequency + 1, key))
 
     def get(self, key):
         """Retrieves an entry from the cache system."""
         if key not in self.cache_data:
             return None
-        frequency = heappop(self._least_frequently_used)
+        frequency = heappop(self._least_frequently_used)[0]
         heappush(self._least_frequently_used, (frequency + 1, key))
         return self.cache_data.get(key)
