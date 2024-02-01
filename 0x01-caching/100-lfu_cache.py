a3b30bdb5152dd975988a91aca95514d96b7eebe
Blxee <aminedev57@gmail.com>
2024-02-01 01:41:50 +0100

    Fixed: task (5. LFU caching) of 0x01. Caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -15,20 +15,30 @@ class LFUCache(BaseCaching):
         """Puts a new entry into the cache system."""
         if key is None or item is None:
             return
-        if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+
+        if ((len(self.cache_data) >= BaseCaching.MAX_ITEMS)
+                and (key not in self.cache_data)):
             rmkey = heappop(self._least_frequently_used)[1]
             del self.cache_data[rmkey]
             print('DISCARD:', rmkey)
+
+        freqency = 0
+        for i, (f, k) in enumerate(self._least_frequently_used):
+            if k == key:
+                self._least_frequently_used.pop(i)
+                freqency = f
+        heappush(self._least_frequently_used, (freqency + 1, key))
+
         self.cache_data[key] = item
-        frequency = 0
-        if key in map(lambda x: x[1], self._least_frequently_used):
-            frequency = heappop(self._least_frequently_used)[0]
-        heappush(self._least_frequently_used, (frequency + 1, key))
 
     def get(self, key):
         """Retrieves an entry from the cache system."""
         if key not in self.cache_data:
             return None
-        frequency = heappop(self._least_frequently_used)[0]
-        heappush(self._least_frequently_used, (frequency + 1, key))
+        freqency = 0
+        for i, (f, k) in enumerate(self._least_frequently_used):
+            if k == key:
+                self._least_frequently_used.pop(i)
+                freqency = f
+        heappush(self._least_frequently_used, (freqency + 1, key))
         return self.cache_data[key]
