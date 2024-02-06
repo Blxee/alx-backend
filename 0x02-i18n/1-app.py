7dc25842076872b9c4dc53ac71295885d8eb058b
Blxee <aminedev57@gmail.com>
2024-02-06 12:39:02 +0100

    Fixed: task (1. Basic Babel setup) of 0x02. i18n

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,11 +4,15 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 
+class Config:
+    LAGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
 app = Flask(__name__)
+app.config.from_object(Config)
 babel = Babel(app)
-app.config['LAGUAGES'] = ['en', 'fr']
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
 
 
 @app.route('/')
