533f1731c8fee6b423ea211ed04f15238f2f99cf
Blxee <aminedev57@gmail.com>
2024-03-07 00:42:44 +0100

    Added: task 10 of 0x03. Queuing systems in js

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,14 @@
+import kue from 'kue';
+
+module.exports = function createPushNotificationsJobs(jobs, queue) {
+  if (!(jobs instanceof Array)) {
+    throw new Error('Jobs is not an array');
+  }
+  for (const jobData of jobs) {
+    const job = queue.create('push_notification_code_3', jobData).save();
+    job.on('enqueue', () => console.log('Notification job created:', job.id))
+      .on('complete', () => console.log(`Notification job ${job.id} completed`))
+      .on('failed', (err) => console.log(`Notification job ${job.id} failed:`, err))
+      .on('progress', (progress) => console.log(`Notification job ${job.id} ${progress}% complete`, err));
+  }
+}
